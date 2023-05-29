dc1d3d54f0a63798c7fb96464c26ccf0ecf98991
Brianmax <george.maxi@ucsp.edu.pe>
2023-05-29 00:29:07 -0500

    createsuperuser fixed

--- users/models.py
+++ users/models.py
@@ -16,24 +16,28 @@ class CustomManagerModel(BaseUserManager):
         user.save()
         return user
 
-    def create_superuser(self, email, password=None, admin_pass=None,
+    def create_superuser(self, dni, email, password=None, admin_pass=None,
                          **extra_fields):
         if not admin_pass:
             raise ValueError('You must provide an admin pass to be admin')
 
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
-        return self.create_superuser(email, password,
-                                     admin_pass, **extra_fields)
+        extra_fields['role'] = 'admin'
+        return self.create_user(dni,
+                                email,
+                                password,
+                                admin_pass,
+                                **extra_fields)
 
 
 class CustomUserModel(AbstractUser):
     STUDENT = 1
     TEACHER = 2
-    ADMINISTRADOR = 3
+    ADMIN = 3
     ROLE_CHOICES = ((STUDENT, 'student'),
-                    (TEACHER, 'Teacher'),
-                    (ADMINISTRADOR, 'Admin'))
+                    (TEACHER, 'teacher'),
+                    (ADMIN, 'admin'))
     dni = models.AutoField(primary_key=True)
     email = models.EmailField(unique=True)
     department = models.CharField(max_length=30)
@@ -42,13 +46,17 @@ class CustomUserModel(AbstractUser):
     number_of_books_allowed = models.BooleanField(default=1)
     cupon = models.BooleanField(default=False)
     # only if the user provides the admin_pass will be admin
-    admin_pass = models.CharField(default=None, max_length=10)
+    admin_pass = models.CharField(default=None, max_length=10, null=True)
     role = models.CharField(max_length=10,
                             choices=ROLE_CHOICES, default='student')
+    REQUIRED_FIELDS = ['email', 'admin_pass', 'dni']
     objects = CustomManagerModel()
 
-    def save(self, *args, **kwargs):
-        if self.role == 'student':
-            self.number_of_books_allowed = 2
-        elif self.role == 'teacher' or self.role == 'admin':
-            self.number_of_books_allowed = 4
+    # def save(self, *args, **kwargs):
+    #    if self.role == 'student':
+    #        self.number_of_books_allowed = 2
+    #    elif self.role == 'teacher' or self.role == 'admin':
+    #        self.number_of_books_allowed = 4
+
+    def __str__(self):
+        return self.email
